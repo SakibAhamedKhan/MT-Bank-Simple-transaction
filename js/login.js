42f6ffdb849eda1595e819972b83f59379d91e0c
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-02-10 23:48:33 +0600

    user update 1

--- js/login.js
+++ js/login.js
@@ -8,24 +8,7 @@ document.getElementById('login-submit').addEventListener('click', function(){
 		window.location.href = 'banking.html?loginSuccess=1';
 		
 	}
-	toastr.options = {
-		"closeButton": true,
-		"debug": false,
-		"newestOnTop": false,
-		"progressBar": true,
-		"positionClass": "toast-bottom-center",
-		"preventDuplicates": false,
-		"onclick": null,
-		"showDuration": "300",
-		"hideDuration": "1000",
-		"timeOut": "5000",
-		"extendedTimeOut": "1000",
-		"showEasing": "swing",
-		"hideEasing": "linear",
-		"showMethod": "fadeIn",
-		"hideMethod": "fadeOut"
-	      }
-	toastr["success"]("Successfully", "Login")
+	
 })
 
 //log out Toast
